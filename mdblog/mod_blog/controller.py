da2e9647e56eab309ad05ca980fefb277a44ec0c
Simona <simabrejcak@gmail.com>
2021-06-23 12:24:19 +0000

    adds erors

---
+++ mdblog/mod_blog/controller.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+from flask.blueprints import Blueprint
+
+from mdblog.models import Article
+
+blog = Blueprint("blog", __name__)
+
+@blog.route("/articles/", methods=["GET"])
+def view_articles():
+    articles = Article.order_by(Article.id.desc())
+    return render_template("mod_blog/articles.jinja", articles=articles)
+
+@blog.route("/articles/<int:art_id>/")
+def view_article(art_id):
+    article = Article.query.filter_by(id=art_id).first()
+    if article:
+        return render_template("mod_blog/article.jinja", article=article)
+    return render_template("mod_blog/article_not_found.jinja", art_id=art_id)
+
+    
