b3ccd1f397e6873f38dd2cc2e1e880b5d0a68d04
Simona <simabrejcak@gmail.com>
2021-08-29 10:51:04 +0000

    fix111

--- configs/development.py
+++ configs/development.py
@@ -1,5 +1,7 @@
 import os
 
+SECRET_KEY = b'E\x9e*0D\xad\x1fX\x85i\x8a\xe5I\xd3\xa0Xhm\x14\xa7\xf7=\x87}'
+
 DEBUG = True
 # DATABASE = "/vagrant/blog.db"
 SQLALCHEMY_DATABASE_URI = "sqlite:////vagrant/blog.db"
