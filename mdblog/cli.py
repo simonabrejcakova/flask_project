88af270506073873b6dfe25f80ca5f2b1849534e
Simona <simabrejcak@gmail.com>
2021-09-14 18:21:55 +0000

    helpdd

--- mdblog/cli.py
+++ mdblog/cli.py
@@ -1,3 +1,4 @@
+
 from mdblog.factory import create_flask_app
 from mdblog.models import db
 from mdblog.models import User
@@ -9,3 +10,10 @@ def init_db(app):
     with app.app_context():
         db.create_all()
         print("database inicialized")
+
+        default_user = User(username="admin")
+        default_user.set_password("admin")
+
+        db.session.add(default_user)
+        db.session.commit()
+        print("default user was created")
